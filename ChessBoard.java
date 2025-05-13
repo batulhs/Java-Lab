636881d87d3d8775fa4a33a517974e4fe15ae8ad
Batul H Suratwala <batulhs2005@gmail.com>
2025-05-13 15:12:26 +0530

    Create ChessBoard.java

---
+++ ChessBoard.java
@@ -0,0 +1,17 @@
+package BM23AI041;
+
+public class ChessBoard {
+    public static void main(String[] args){
+        int size=8;
+        for (int i=0;i<8;i++){
+            for (int j=0;j<8;j++){
+                if ((i+j)%2==0){
+                    System.out.print("**");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println("\n");
+        }
+    }
+}
