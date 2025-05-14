590c3e01b09e3a0b0e948888e5ed10170ee79029
Batul H Suratwala <batulhs2005@gmail.com>
2025-05-14 15:51:23 +0530

    Update (Lab7)PharmacyManagementSystem.java

--- (Lab7)PharmacyManagementSystem.java
+++ (Lab7)PharmacyManagementSystem.java
@@ -2,7 +2,6 @@ package BM23AI041;
 
 import java.util.ArrayList;
 
-
 class Pharmacy {
     public final int id;
     public final String name;
@@ -22,14 +21,10 @@ class Pharmacy {
 public class PharmacyManagementSystem {
     private ArrayList<Pharmacy> inventory = new ArrayList<>();
 
-
-    // Add new item to inventory
     public void addItem(Pharmacy item) {
         inventory.add(item);
     }
 
-
-    // Update existing item in inventory
     public void updateItem(int id, int quantity, double price) {
         for (Pharmacy item : inventory) {
             if (item.id == id) {
@@ -39,13 +34,11 @@ public class PharmacyManagementSystem {
             }
         }
     }
-    // Remove item from inventory
+
     public void removeItem(int id) {
         inventory.removeIf(item -> item.id == id);
     }
 
-
-    // Display list of all items
     public void displayInventory() {
         System.out.println("Inventory:");
         for (Pharmacy item : inventory) {
@@ -53,41 +46,25 @@ public class PharmacyManagementSystem {
         }
     }
 
-
-    // Search for item by ID
     public Pharmacy searchItemByID(int id) {
         for (Pharmacy item : inventory) {
             if (item.id == id) {
                 return item;
             }
         }
-        return null; // Item not found
+        return null; 
     }
 
 
     public static void main(String[] args) {
         PharmacyManagementSystem ims = new PharmacyManagementSystem();
 
-
-        // Adding sample items
         ims.addItem(new Pharmacy(1, "Crocin", 100, 2.5));
         ims.addItem(new Pharmacy(2, "Calpol", 50, 3.0));
         ims.addItem(new Pharmacy(3, "Crickmol", 200, 1.0));
-
-
-        // Displaying inventory
         ims.displayInventory();
-
-
-        // Updating an item
         ims.updateItem(1, 90, 2.0);
-
-
-        // Displaying inventory after update
         ims.displayInventory();
-
-
-        // Searching for an item
         System.out.println("Searching for item with ID 2:");
         Pharmacy foundItem = ims.searchItemByID(2);
         if (foundItem != null) {
@@ -95,13 +72,7 @@ public class PharmacyManagementSystem {
         } else {
             System.out.println("Item not found.");
         }
-
-
-        // Removing an item
         ims.removeItem(2);
-
-
-        // Displaying inventory after removal
         ims.displayInventory();
     }
 }
