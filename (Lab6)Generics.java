53fe5ec43a7b9ec6bcd9e1f4fe2622058bc212f8
Batul H Suratwala <batulhs2005@gmail.com>
2025-05-14 15:49:46 +0530

    Update (Lab6)Generics.java

--- (Lab6)Generics.java
+++ (Lab6)Generics.java
@@ -31,21 +31,14 @@ public class GQueue<T> {
         }
     }
     public static void main(String[] args) {
-        // Create a GrowableQueue of integers
         GQueue<Integer> integerQueue = new GQueue<>();
-        // Enqueue elements
         integerQueue.enqueue(10);
         integerQueue.enqueue(20);
         integerQueue.enqueue(30);
-
         System.out.println("ELements in the queue are:");
         integerQueue.display();
-        // Dequeue an element
         int dequeuedElement = integerQueue.dequeue();
         System.out.println("Dequeued element: " + dequeuedElement);
-
-
-        // Display size of the queue
         System.out.println("Queue size: " +(++size));
 
     }
